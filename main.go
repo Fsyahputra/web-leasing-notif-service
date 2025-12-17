fa7f98d3b82ca1d0040c2c655d3eb9b5e92bc5dc
fadhil <syahputraf112@gmail.com>
2025-12-17 16:28:42 +0800

    add send whatsapp functionalty

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/IBM/sarama"
 )
+
+type ReqBody struct {
+	Msg       string `json:"msg"`
+	SessionId string `json:"session_id"`
+	To        string `json:"to"`
+}
+
+func configureReq(req *http.Request) {
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Tenant", "huntersmithnusantara")
+
+}
+
+func sendToWhatsapp(msg string) {
+	apiUrl := "https://api-wa.onestopcheck.id/api/whatsapp/send/text-notif"
+	reqBody := ReqBody{
+		Msg:       msg,
+		SessionId: "your-session-id",
+		To:        "6282250712167@c.us",
+	}
+	jsonData, _ := json.Marshal(reqBody)
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", apiUrl, bytes.NewBuffer(jsonData))
+	configureReq(req)
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	fmt.Println("Response Status:", resp.Status)
+
+}
 
 func main() {
 	brokerList := []string{"localhost:9092"}
@@ -27,5 +59,6 @@ func main() {
 	fmt.Println("Consuming messages from test-topic...")
 	for msg := range consumer.Messages() {
 		fmt.Printf("Message: %s\n", string(msg.Value))
+		sendToWhatsapp(string(msg.Value))
 	}
 }
